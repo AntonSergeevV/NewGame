a0b036252a67662667f0e746af9d0bfcf9b4c50d
Антон <sergeev.tony@ya.ru>
2023-11-06 19:35:21 +0300

    style

---
+++ src/main/java/com/example/newgame/HelloController.java
@@ -0,0 +1,46 @@
+package com.example.newgame;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView bg1, bg2, player;
+
+    private final int BG_WIGHT = 714;
+
+    public ParallelTransition parallelTransition;
+
+    @FXML
+    void initialize() {
+        TranslateTransition bgOneTransition = new TranslateTransition(Duration.millis(5000), bg1);
+        bgOneTransition.setFromX(0);
+        bgOneTransition.setToX(BG_WIGHT * -1);
+        bgOneTransition.setInterpolator(Interpolator.LINEAR);
+
+        TranslateTransition bgTwoTransition = new TranslateTransition(Duration.millis(5000), bg2);
+        bgTwoTransition.setFromX(0);
+        bgTwoTransition.setToX(BG_WIGHT * -1);
+        bgTwoTransition.setInterpolator(Interpolator.LINEAR);
+
+        parallelTransition = new ParallelTransition(bgOneTransition, bgTwoTransition);
+        parallelTransition.setCycleCount(Animation.INDEFINITE);
+        parallelTransition.play();
+    }
+
+}
